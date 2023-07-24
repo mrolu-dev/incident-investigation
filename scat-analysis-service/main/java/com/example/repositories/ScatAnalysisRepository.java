1c9cff97d6b2d0989d190fccbb517735b6533a63
Olu <o.olumoyin@yahoo.com>
2023-07-24 00:20:11 -0600

    V3

--- scat-analysis-service/main/java/com/example/repositories/ScatAnalysisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repositories;
-
-import com.example.models.ScatAnalysis;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ScatAnalysisRepository extends MongoRepository<ScatAnalysis, String> {
-    // You can define custom query methods here if needed
-}
